5d453d5bb2b87cb382568d91a0a810ec0ecf650d
Mateusz Derda <mateuszderda95@gmail.com>
2019-05-10 16:20:18 +0200

    Stworzone pierwsze funkcjonalnosci

--- pinterest/urls.py
+++ pinterest/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import IndexView, AddPhotoView
+from django.conf import settings
+
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', IndexView.as_view()),
+    path('add_photo', AddPhotoView.as_view()),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
